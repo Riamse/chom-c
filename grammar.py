d711c58fbf9b66a89f23c4ba68e2697c20298f08
Riamse <andrewwang43@gmail.com>
2015-12-31 02:15:56 -0800

    Update the inner workings of the [[generative grammar] generator]

--- grammar.py
+++ grammar.py
@@ -4,16 +4,65 @@
 
 import sys
 import collections
-import random
+
+TWO_ZILLION = (2 ** 30 - 1) * 2 + 1
 
 sys.setrecursionlimit(2 ** 10)
 
+
+class ProbabilityDistribution:
+    def __init__(self, name="X"):
+        self.name = name
+        self.crapmap = {}
+        self.rvcrapmap = {}
+        self.crap = {}
+
+    def __repr__(self):
+        return "{.__name__}(name={!r})".format(type(self), self.name)
+
+    def _behasherise(self, crap):
+        v = hash(crap) % TWO_ZILLION
+        if v in self.rvcrapmap:
+            return self._behasherise(str(v))
+        return v
+
+    def __setitem__(self, val, p):
+        h = self.crapmap.get(val, self._behasherise(val))
+        self.crapmap[val] = h
+        self.rvcrapmap[h] = val
+        self.crap[h] = p
+
+    def __getitem__(self, val):
+        # returns P(X = val) for a random variable X ~ self
+        return self.crap[val]
+
+    def __call__(self, n=1):
+        #if sum(self.crap.values()) != 1:
+        #    raise TypeError("not a real distribution")
+        garbage = [], []
+        for k, v in self.crap.items():
+            garbage[0].append(k)
+            garbage[1].append(v)
+        from scipy.stats import rv_discrete  # overkill, yo
+        X = rv_discrete(values=garbage)
+        realret = [None] * n
+        for i, h in enumerate(X.rvs(size=n)):
+            realret[i] = self.rvcrapmap[h]
+        return realret
+
+
 class Grammar:
-    """do everything"""
+    """This does like everything"""
     def __init__(self):
         self.terms = set()
         self.nonterms = set()
-        self.rules = collections.defaultdict(list)
+        self.rules = {}
+
+    def add_rule(self, nonterm: str, term: dict):
+        X = ProbabilityDistribution(nonterm)
+        for expansion, p in term.items():
+            X[expansion] = p
+        self.rules[nonterm] = X
 
     def add_terminals(self, terminals: set):
         assert not terminals & self.nonterms
@@ -32,7 +81,7 @@ class Grammar:
     def expand(self, nonterm, tab=0):
         assert nonterm in self.nonterms
         ret = []
-        tokens = random.choice(self.rules[nonterm])
+        tokens = self.rules[nonterm]()[0]
         for tok in tokens:
             if tok in self.terms:
                 ret.append(tok)
