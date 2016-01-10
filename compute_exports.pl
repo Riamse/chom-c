4096f36699f66bf34066cf0daf7954fcc7191a25
Riamse <andrewwang43@gmail.com>
2016-01-09 20:46:20 -0800

    Minor typo fix again

--- compute_exports.pl
+++ compute_exports.pl
@@ -22,7 +22,7 @@ foreach my $dot_h (@$HEADERS) {
     my $script;
     $text = `perl get_exports.pl $dot_h`;
     chomp($dot_h = Dumper $dot_h);
-    $dot_h = substr($dot_h, 8, -1);
+    $dot_h = substr($dot_h, 8, -1) . '.pkl';
     #$dot_h =~ s/(;|\$VAR1 = )//g;
     $script = sprintf $TPL, $text, $dot_h;
     system("python3", "-c", $script);
