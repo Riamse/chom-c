fd2bab19e22039ad751aa4949d0b3b9e318d3d4a
Riamse <andrewwang43@gmail.com>
2016-01-11 16:35:36 -0800

    Grammatical musings, tweak the example grammar
    README.md: Reminder that context stuff is extremely explosive.
    
    example.py: Intransitive verbs should appear less often. Tweak the
    notation of nonterminals to be consistent with the C grammar.

--- README.md
+++ README.md
@@ -4,3 +4,8 @@ A simple generative grammar for C code.
 You'll first want to run `mkdir header_exports`, and then `perl compute_exports.pl`. Then you can run `python -i c.py` and do things to `g` as you wish.
 
 An instance of `Grammar` maintains context in its own attributes. If you want to expand non-terminals in parallel, you need to create another `Grammar` object. `c.py` defines a function, `new_c()` to facilitate this process.
+
+This project also doubles as a general framework for phrase structure grammar. As the context-tracking system was developed with programming languages in mind, adapting that specific feature for use with natural language is an exercise left to semantic syntacticians.
+
+`example.py` contains a simple subset of English that does not make use of the context-tracking features.
+
--- example.py
+++ example.py
@@ -1,34 +1,37 @@
-if True:
-    rules = {
-        "S": {
-            ("NP", "VP"): 1
-        },
-        "NP": {
-            ("A", "NP"): .3,
-            ("N",): .7,
-        },
-        "VP": {
-            ("V", "NP"): .5,
-            ("V",): .5,
-        },
-        "N": {
-            ('ideas',): .5,
-            ('linguists',): .5,
-        },
-        "V": {
-            ("hate",): .5,
-            ("generate",): .5,
-        },
-        "A": {
-            ("great",): .5,
-            ("green",): .5,
-        }
+from grammar import Grammar
+
+from fractions import Fraction
+
+rules = {
+    "$S": {
+        ("$NP", "$VP"): 1
+    },
+    "$NP": {
+        ("$A", "$NP"): Fraction(3, 10),
+        ("$N",): Fraction(7, 10),
+    },
+    "$VP": {
+        ("$V", "$NP"): Fraction(3, 4),
+        ("$V",): Fraction(1, 4),
+    },
+    "$N": {
+        ('ideas',): .5,
+        ('linguists',): .5,
+    },
+    "$V": {
+        ("hate",): .5,
+        ("generate",): .5,
+    },
+    "$A": {
+        ("great",): .5,
+        ("green",): .5,
     }
+}
 
-    english = Grammar()
-    english.add_terminals({'generate', 'hate', 'great', 'green', 'ideas', 'linguists'})
-    english.add_nonterminals({"S", 'NP', 'VP', 'N', 'V', 'A'})
-    for k, v in rules.items():
-        english.add_rule(k, v)
+english = Grammar()
+english.add_terminals({'generate', 'hate', 'great', 'green', 'ideas', 'linguists'})
+english.add_nonterminals({"$S", '$NP', '$VP', '$N', '$V', '$A'})
+for k, v in rules.items():
+    english.add_rule(k, v)
 
-    print(english.expand("S"))
+print(english.expand("$S"))
